04f49d2d7472f913046fbdd7c9091fe7d72681c5
dvyas@tvo.org <dvyas@tvo.org>
2024-10-22 10:25:42 -0400

    Initial Commit

---
+++ TheGlobeMailExercise/TheGlobeMailExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGlobeMailExerciseApp.swift
+//  TheGlobeMailExercise
+//
+//  Created by Darshan Vyas on 2024-10-22.
+//
+
+import SwiftUI
+
+@main
+struct TheGlobeMailExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
